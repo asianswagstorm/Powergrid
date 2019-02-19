9fe42f843f7961cb9dc5b929ead6ca6400d4bc8b
Andy Nguyen <brobromario@hotmail.com>
2019-02-19 13:45:15 -0500

    user input for starting and ending index, check dijkstra of any path

--- Part1_Create_Map/main_part1.cpp
+++ Part1_Create_Map/main_part1.cpp
@@ -12,14 +12,16 @@ using std::cout;
 
 int main()
 {
-	vector<int> areas = {0,2,3,5 }; //To test areas in use, enter the areas here
-	Area::setGameAreas(areas);
+	vector<int> areas = {0,2,3,5}; //To test areas in use, enter the areas here
 	//Testing if areas are adjacent
 	
 	if (Area::areAdjacent(areas) == false) {
 		std::cout << "Error, areas are not adjacent!" << std::endl;
 		system("pause");
-		return 0;
+		return 1;
+	}
+	else {
+		Area::setGameAreas(areas);
 	}
 
 	//create map
@@ -27,18 +29,25 @@ int main()
 	
 	//Save map to map.txt
 	IOFile::saveMap();
+	int start_node;
+	int end_node;
+
+	std::cout << "Enter the starting node index: " << std::endl;
+	std::cin >> start_node;
+	std::cout << "Enter the ending node index: " << std::endl;
+	std::cin >> end_node;
 
 	std::vector<weight_t> min_distance;
 	std::vector<vertex_t> previous;
-	test.DijkstraComputePaths(0, *Map::map, min_distance, previous);
+	test.DijkstraComputePaths(start_node, *Map::map, min_distance, previous);
 
-	std::cout << "Distance from 0 to 41: " << min_distance[41] << std::endl;
-	std::list<vertex_t> path = test.DijkstraGetShortestPathTo(41, previous); //list that will contain the paths taken
+	std::cout << "Distance from "<< start_node << " to " << end_node <<": " << min_distance[end_node] << std::endl;
+	std::list<vertex_t> path = test.DijkstraGetShortestPathTo(end_node, previous); //list that will contain the paths taken
 	std::cout << "Path : ";
 	std::copy(path.begin(), path.end(), std::ostream_iterator<vertex_t>(std::cout, " "));
 	std::cout << std::endl;
 
-	std::cout << "Starting City: " + test.getCityName(0) + " Ending City: " + test.getCityName(41)  << std::endl;
+	std::cout << "Starting City: " + test.getCityName(start_node) + " Ending City: " + test.getCityName(end_node)  << std::endl;
 	
 	system("pause");
 	return 0;
