0b7a988c14830156b076250fa93a0f30250e801c
SeifElDerby <seif.derby@gmail.com>
2019-03-06 16:00:28 -0500

    updates

--- mapreader.h
+++ mapreader.h
@@ -30,6 +30,42 @@ public:
 		Area = area;
 	}
 
+	void readMap(){
+		ifstream mapfile ("mapfile.txt");
+		int index;
+		string cityName;
+		string area;
+		string line;
+		int lineCounter=0;
+		string indexHolder;
+
+		if (!mapfile) {
+		        cout << ("Error: text file not found") << endl;
+		        system ("pause");
+		        exit (EXIT_FAILURE);
+		}
+		if(mapfile >> index >>','>> cityName >>','>> area){
+
+			while (getline(mapfile, line) && (line != "") && !line.empty()) {
+				lineCounter++;
+				std::stringstream lineHolder(line);
+
+				getline(lineHolder, indexHolder, ',');
+				index= stoi(indexHolder);
+				getline(lineHolder, cityName, ',');
+				getline(lineHolder, area, ',');
+
+				index.setIndex(index);
+				cityName.setCityName(cityName);
+				area.setArea(area);
+
+			}
+		}
+		else{
+			cout <<("Not a valid map") << endl;
+		}
+	}
+
 };
 
 
